9c5086a23540bf404c3cef7d60a32c7b15adaa95
Gurshaan <138469341+GS-109@users.noreply.github.com>
2026-06-25 09:39:29 +0000

    RandomStuff

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods = ("GET", "POST"))
 def index():
-    return render_template("index.html")
+    time, name = None 
+    if request.method == "POST":
+        name = request.form["Name"]
+        dob = request.form["DOB"]
+        gender = request.form["Gender"]
+        time = "7 Hours"
+    return render_template("index.html", time = time, name = name)
 
 if __name__ == "__main__":
     app.run(debug = True)
